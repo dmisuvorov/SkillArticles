2a8d1e86e1e5bb25d6d8ddadab605c2784360e49
dmitry <dmitrysuvorov9@gmail.com>
2020-02-04 07:06:47 +0300

    Bug fixes

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -3,7 +3,7 @@ package ru.skillbranch.skillarticles.extensions
 fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
     val result = mutableListOf<Int>()
     (if (ignoreCase) substr.toRegex(RegexOption.IGNORE_CASE) else substr.toRegex())
-        .findAll(this ?: "").forEach { matchResult ->
+        .findAll(if (this.isNullOrEmpty()) return emptyList() else this).forEach { matchResult ->
             result.add(matchResult.range.first)
         }
     return result
