d14994dd32e7c5cc6e2883558f7db658d787dd57
dmisuvorov` <dmitrysuvorov9@gmail.com>
2020-07-08 07:31:08 +0300

    Change project structure

--- app/src/main/java/ru/skillbranch/skillarticles/ui/dialogs/ChoseCategoryDialog.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/dialogs/ChoseCategoryDialog.kt
@@ -2,12 +2,20 @@ package ru.skillbranch.skillarticles.ui.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_category.*
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.viewmodels.articles.ArticlesViewModel
 
@@ -67,15 +75,64 @@ class ChoseCategoryDialog : DialogFragment() {
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putStringArray(::selectedCategories.name, selectedCategories.toTypedArray())
         super.onSaveInstanceState(outState)
+        outState.putStringArray(::selectedCategories.name, selectedCategories.toTypedArray())
     }
 
-    data class CategoryItem(
-        val categoryId: String,
-        val icon: String,
-        val title: String,
-        val articlesCount: Int = 0,
-        val checked: Boolean = false
-    )
+}
+
+data class CategoryItem(
+    val categoryId: String,
+    val icon: String,
+    val title: String,
+    val articlesCount: Int = 0,
+    val checked: Boolean = false
+)
+
+
+class CategoriesAdapter(
+    private val listener: (CategoryItem, Boolean) -> Unit
+) : ListAdapter<CategoryItem, CategoryVH>(CategoryDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryVH {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
+        return CategoryVH(view, listener)
+    }
+
+    override fun onBindViewHolder(holder: CategoryVH, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class CategoryVH(
+    override val containerView: View,
+    val listener: (CategoryItem, Boolean) -> Unit
+) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+    fun bind(item: CategoryItem) {
+        ch_select.isChecked = item.checked
+        tv_category.text = item.title
+        tv_count.text = "${item.articlesCount}"
+        Glide.with(containerView.context)
+            .load(item.icon)
+            .into(iv_icon)
+
+        ch_select.setOnCheckedChangeListener { _, checked -> listener(item, checked) }
+    }
+
+}
+
+class CategoryDiffCallback : DiffUtil.ItemCallback<CategoryItem>() {
+    override fun areItemsTheSame(
+        oldItem: CategoryItem,
+        newItem: CategoryItem
+    ): Boolean =
+        oldItem.categoryId == newItem.categoryId
+
+    override fun areContentsTheSame(
+        oldItem: CategoryItem,
+        newItem: CategoryItem
+    ): Boolean =
+        oldItem == newItem
+
 }
